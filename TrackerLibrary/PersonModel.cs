c76dd20ead23b6923f41a7d9e4873763c24fe9da
William Matheus <wgtlwill@gmail.com>
2022-03-13 21:59:31 -0300

    Add project files.

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+	public class PersonModel
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailAdress { get; set; }
+		public string Cellphone { get; set; }
+	}
+}
